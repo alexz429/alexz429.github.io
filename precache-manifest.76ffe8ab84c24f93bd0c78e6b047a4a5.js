71c65ade038a6f7e4ee49776a33c6c0e51b2d765
alexz429 <alexz2002429@gmail.com>
2020-02-23 22:17:20 -0500

    deployment page

---
+++ precache-manifest.76ffe8ab84c24f93bd0c78e6b047a4a5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e9778b3a48cf3b2a4a098ae2a9e2e92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e15d7ae72fce173b1b1",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "2e0810e1daf696d11267",
+    "url": "/static/css/main.1ce2553d.chunk.css"
+  },
+  {
+    "revision": "5e15d7ae72fce173b1b1",
+    "url": "/static/js/2.e67a72f2.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/static/js/2.e67a72f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e0810e1daf696d11267",
+    "url": "/static/js/main.cfdc7f45.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  },
+  {
+    "revision": "f22166a6d68ac6bcc4bd46ec2bfe112a",
+    "url": "/static/media/bg.f22166a6.jpg"
+  }
+]);
